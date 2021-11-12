52bec4d6d4ed1904308263a7ceea377d8e42ef61
sapphi-red <green@sapphi.red>
2021-11-12 11:27:37 +0900

    Update: add unpkg test

--- test/main.js
+++ test/main.js
@@ -5,55 +5,9 @@ import { EnumNamesPlugin } from 'swagger-ui-plugin-enum-names'
 import 'swagger-ui/dist/swagger-ui.css'
 import 'swagger-ui-plugin-enum-names/dist/index.min.css'
 
-const spec = {
-  openapi: '3.0.0',
-  paths: {
-    '/items': {
-      get: {
-        responses: {
-          200: {
-            content: {
-              'application/json': {
-                schema: {
-                  type: 'array',
-                  items: {
-                    $ref: '#/components/schemas/Items'
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  },
-  components: {
-    schemas: {
-      Items: {
-        type: 'object',
-        properties: {
-          id: {
-            type: 'integer',
-            format: 'int32'
-          },
-          type: {
-            $ref: '#/components/schemas/ItemType'
-          }
-        }
-      },
-      ItemType: {
-        type: 'string',
-        enum: ['General', 'Special'],
-        'x-enum-varnames': ['general', 'special'],
-        'x-enum-descriptions': ['general-itemtype', 'special-itemtype']
-      }
-    }
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   SwaggerUI({
-    spec,
+    url: '/spec.json',
     dom_id: '#app',
     deepLinking: true,
     presets: [SwaggerUI.presets.apis, SwaggerUIStandalonePreset],
