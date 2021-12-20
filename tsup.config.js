8d95ba0cfb6a657fab407507a95f044fd3b9bda8
sapphi-red <green@sapphi.red>
2021-12-21 00:10:41 +0900

    Chore: change target

--- tsup.config.js
+++ tsup.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('tsup')
 
 module.exports = defineConfig((options) => ({
   entry: ['src/index.js'],
+  platform: 'browser',
+  target: 'es2015',
   sourcemap: true,
   minify: !options.watch,
   format: ['cjs', 'esm', 'iife'],
