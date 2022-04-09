7aa69ca5bb49a945153903201ba1df06ee12d077
sapphi-red <green@sapphi.red>
2022-04-09 20:56:13 +0900

    Chore: update test packages

--- test/main.js
+++ test/main.js
@@ -1,5 +1,5 @@
 import SwaggerUI from 'swagger-ui'
-import SwaggerUIStandalonePreset from 'swagger-ui/dist/swagger-ui-standalone-preset'
+import { SwaggerUIStandalonePreset } from 'swagger-ui-dist'
 import { EnumNamesPlugin } from 'swagger-ui-plugin-enum-names'
 
 import 'swagger-ui/dist/swagger-ui.css'
