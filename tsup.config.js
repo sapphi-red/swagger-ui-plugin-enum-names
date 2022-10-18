c263ca548864e6ba983bdea8f08ecb30f65a58b1
sapphi-red <green@sapphi.red>
2022-10-18 19:07:04 +0900

    Chore: remove sourcemap other than iife's one

--- tsup.config.js
+++ tsup.config.js
@@ -1,13 +1,21 @@
-const { defineConfig } = require('tsup')
+const { defineConfig } = require("tsup")
 
-module.exports = defineConfig((options) => ({
-  entry: ['src/index.js'],
-  platform: 'browser',
-  target: 'es2015',
-  sourcemap: true,
-  minify: !options.watch,
-  format: ['cjs', 'esm', 'iife'],
-  esbuildOptions(options, context) {
-    options.globalName = 'EnumNamesPlugin'
-  }
-}))
+module.exports = defineConfig((options) => {
+  const common = defineConfig({
+    entry: ["src/index.js"],
+    platform: "browser",
+    target: "es2015",
+    globalName: "EnumNamesPlugin",
+  })
+
+  return ["cjs", "esm", "iife"].map((format) =>
+    defineConfig({
+      ...common,
+      sourcemap: format === "iife",
+      minifyIdentifiers: !options.watch,
+      minifySyntax: !options.watch,
+      minifyWhitespace: !options.watch && format === "iife",
+      format,
+    })
+  )
+})
